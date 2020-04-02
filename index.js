b8ceb6bb800164d9554ebd55d1e8ad2d02db2112
Floyed Pinto <pinto.fl@husky.neu.edu>
2020-04-02 01:20:51 -0400

    Adding timestamp related changes

--- index.js
+++ index.js
@@ -1,92 +1,91 @@
 const aws = require("aws-sdk");
-var ddb = new aws.DynamoDB({ apiVersion: "2012-08-10" });
-var rdsdb = new aws.RDS({ apiVersion: "2012-08-10" });
+var ddb = new aws.DynamoDB({ apiVersion: '2012-08-10' });
+var rdsdb = new aws.RDS();
 var ses = new aws.SES();
 aws.config.update({ region: "us-east-1" });
 var docClient = new aws.DynamoDB.DocumentClient();
 
-const mysql = require('mysql2');
-
-const connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password:process.env.RDS_PASSWORD,
-    database: 'billmanagement'
-});
-
 
 exports.billDueService = function (event, context, callback) {
 
-    let message = event.Records[0].Sns.Message;
-    let messageJson = JSON.parse(message);
-    let messageDataJson = JSON.parse(messageJson.data);
+    console.log(event.Records[0].Sns);
 
-    console.log("Test Message: " + messageJson.data);
-    console.log("Test Link: " + messageDataJson.link);
-    console.log("Test Email: " + messageDataJson.Email);
-    
+    let message = event.Records[0].Sns.Message;
+    let messageDataJson = JSON.parse(JSON.parse(message).data);
+
+    let email = messageDataJson.Email;
+    let link = messageDataJson.link;
+
+    link = link.replace('undefined', 'prod.floyed-pinto.me');
+
+    console.log("Email for :: " + email);
+    console.log("Link to send :: " + link);
+
     let currentTime = new Date().getTime();
     let ttl = 15 * 60 * 1000;
     let expirationTime = (currentTime + ttl).toString();
+
     var emailParams = {
         Destination: {
-            /* required */
             ToAddresses: [
-                messageDataJson.Email
-                /* more items */
+                email
             ]
         },
         Message: {
-            /* required */
             Body: {
                 Text: {
                     Charset: "UTF-8",
-                    Data: messageDataJson.link
+                    Data: link
                 }
             },
             Subject: {
                 Charset: "UTF-8",
-                Data: "Password Reset Link"
+                Data: "Bills due"
             }
         },
-        Source: "csye6225@" + process.env.DOMAIN_NAME /* required */
+        Source: "floyedpinto08@" + process.env.DOMAIN_NAME
     };
     let putParams = {
-        TableName: "csye6225",
+        TableName: "emailrequest",
         Item: {
-            id: { S: messageDataJson.Email },
-            resetlink: { S: messageDataJson.link },
+            id: { S: email },
             ttl: { N: expirationTime }
         }
     };
     let queryParams = {
-        TableName: 'csye6225',
+        TableName: 'emailrequest',
         Key: {
-            'id': { S: messageDataJson.Email }
+            'id': { S: email }
         },
     };
-    // first get item and check if email exists
-    //if does not exist put item and send email,
-    //if exists check if ttl > currentTime,
-    // if ttl is greater than current time do nothing,
-    // else send email
+
+
     ddb.getItem(queryParams, (err, data) => {
-        if (err) console.log(err)
-        else {
-            // console.log('getItemttl: '+JSON.stringify(data, null, 2));
-            console.log(data.Item)
-            let jsonData = JSON.stringify(data)
-            console.log(jsonData)
+
+        console.log("Data ::" + JSON.stringify(data));
+
+        if (err) {
+            console.log(err);
+        } else {
+
+            console.log(data.Item);
+
+            let jsonData = JSON.stringify(data);
+            console.log(jsonData);
+
             let parsedJson = JSON.parse(jsonData);
-            console.log(parsedJson)
-            if (data.Item == null) {
+            console.log(parsedJson);
+            if (data.Item == undefined) {
+
                 ddb.putItem(putParams, (err, data) => {
-                    if (err) console.log(err);
-                    else {
+                    if (err) {
+                        console.log(err);
+                    } else {
                         console.log(data);
-                        console.log('sent from 1st function')
-                        var sendPromise = ses.sendEmail(emailParams).promise();
-                        sendPromise
+
+                        console.log('Email sent for first time!');
+
+                        ses.sendEmail(emailParams)
                             .then(function (data) {
                                 console.log(data.MessageId);
                             })
@@ -97,18 +96,26 @@ exports.billDueService = function (event, context, callback) {
                 });
             } else {
                 let curr = new Date().getTime();
+
                 let ttl = Number(parsedJson.Item.ttl.N);
+
                 console.log(typeof curr + ' ' + curr);
                 console.log(typeof ttl + ' ' + ttl);
+
                 if (curr > ttl) {
 
                     ddb.putItem(putParams, (err, data) => {
-                        if (err) console.log(err);
-                        else {
+
+                        console.log("Data putitem::" + data);
+
+                        if (err) {
+                            console.log(err);
+                        } else {
+
                             console.log(data);
-                            console.log('sent from 1st function')
-                            var sendPromise = ses.sendEmail(emailParams).promise();
-                            sendPromise
+                            console.log('Updating last sent timestamp');
+
+                            ses.sendEmail(emailParams)
                                 .then(function (data) {
                                     console.log(data.MessageId);
                                 })
@@ -117,6 +124,8 @@ exports.billDueService = function (event, context, callback) {
                                 });
                         }
                     });
+                } else {
+                    console.log('Email already sent in the last 60 mins for user ::'+email);
                 }
             }
         }
